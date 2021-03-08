aedddf1cea702970eaab6e3d21aa1c289122e268
Pupsky <80257933+Pupsky@users.noreply.github.com>
2021-03-08 19:52:02 +0400

    Файлы сайта

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function(){
+$('.buttonmenu').click(function(){
+$('.menu').toggleClass('show-menu')
+$('.registr').toggleClass('show-menu2')
+})
+});
+//код открытия меню в версиях с маленьким разрешением
