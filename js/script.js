77360a78b5f35490227b90e41a992aa869aac5d3
Pupsky <80257933+Pupsky@users.noreply.github.com>
2021-04-14 21:52:06 +0400

    Delete script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-$('.buttonmenu').click(function(){
-$('.menu').toggleClass('show-menu')
-$('.registr').toggleClass('show-menu2')
-})
-});
-//код открытия меню в версиях с маленьким разрешением
